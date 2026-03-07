d0024ebd54225078ac449f39a27e9ae11422a94e
makingmodels <mkumar@aventur.co.uk>
2026-03-07 16:23:51 +0000

    Fix Memory model scene cloning and reset position/scale

--- app/components/models/Memory.tsx
+++ app/components/models/Memory.tsx
@@ -1,9 +1,11 @@
 import React, { JSX } from 'react'
 import { useGLTF } from '@react-three/drei'
+import { useMemo } from 'react'
 
 export function Memory(props: JSX.IntrinsicElements['group']) {
   const { scene } = useGLTF('models/dalithe_persistence_of_memory.glb')
-  return <primitive object={scene} {...props} dispose={null} />
+  const cloned = useMemo(() => scene.clone(), [scene])
+  return <primitive object={cloned} {...props} dispose={null} />
 }
 
 useGLTF.preload('models/dalithe_persistence_of_memory.glb')
