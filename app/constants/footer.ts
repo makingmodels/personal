cfa7b8cb95e66c38b724aa5436e3dab7ff2b267b
makingmodels <mkumar@aventur.co.uk>
2026-03-08 12:06:23 +0000

    Update footer, opengraph image, resume, and work skills

--- app/constants/footer.ts
+++ app/constants/footer.ts
@@ -5,30 +5,30 @@ export const FOOTER_LINKS: FooterLink[] = [
     name: 'LinkedIn',
     hoverText: 'Connect with me',
     icon: 'icons/linkedin.svg',
-    url: 'https://www.linkedin.com/in/mohit-virli-4780b6112/',
+    url: 'https://www.linkedin.com/in/mansakumar/',
   },
   {
     name: 'GitHub',
     hoverText: 'Open Sourcing',
     icon: 'icons/github.svg',
-    url: 'https://github.com/mohitvirli',
+    url: 'https://github.com/makingmodels',
   },
   {
-    name: 'Spotify',
-    hoverText: 'Curated playlists',
+    name: 'Soon',
+    hoverText: 'Curated projects',
     icon: 'icons/spotify.svg',
-    url: 'https://open.spotify.com/user/21hr4w2hzp4ceidewwsb4bxoy',
+    url: 'https://www.linkedin.com/in/mansakumar/',
   },
   {
-    name: 'Instagram',
+    name: 'google',
     hoverText: '@clevirli',
     icon: 'icons/instagram.svg',
-    url: 'https://www.instagram.com/clevirli/',
+    url: 'https://google.com',
   },
   {
     name: 'Resume',
     hoverText: 'Download',
     icon: 'icons/file.svg',
-    url: './Mohit Virli - Resume.pdf',
+    url: './Mansa Kumar - Resume.pdf',
   }
 ];
